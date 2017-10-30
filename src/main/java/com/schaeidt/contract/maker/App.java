6ae0cb1b67e7a437a8eb829e47c84df072006b3d
Valentin Schaeidt <vschaeidt@hotmail.fr>
2017-10-30 11:17:39 +0100

    initial commit

---
+++ src/main/java/com/schaeidt/contract/maker/App.java
@@ -0,0 +1,18 @@
+package com.schaeidt.contract.maker;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in); 
+        scanner.nextLine();
+        scanner.close();
+    }
+}
